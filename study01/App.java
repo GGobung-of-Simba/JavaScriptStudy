281b5462b119c9b119da9ca293314cb39b106cb1
hi_jieun <ghghf93@naver.com>
2024-12-31 12:07:08 +0900

    자바와 자바스크립트 차이 2

--- study01/App.java
+++ study01/App.java
@@ -6,10 +6,32 @@ public class App {
         System.out.println("안녕하세요.");
         int 정수 = 1;
         //final int 상수 = 2;
+    
+        모듈 객체 = new 모듈();
+        System.out.println( 객체.기능1(1,4) );
+
+        int a = 1;
+        String b = "1";
+        // if (a==b){}
+        // 자바는 숫자와 문자를 비교 할 수 없음
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+
+    // int add(int a, int b){
+    //     return a + b;
+    // }
+
     }
-    int add(int a, int b){
+}
+class 모듈 {
+    public int 기능1(int a, int b){
         return a + b;
     }
-    // 접근제한자 없는 메소드 하나 만든거임
-        
+    public int 기능2(int a, int b){
+        return a + b;
+    }
+    public int 기능3(int a, int b){
+        return a + b;
+    }
 }
--- study01/App.js
+++ study01/App.js
@@ -19,8 +19,8 @@ const add3 = add;
 const add2 = (a,b) => a + b;
 // 이렇게 쓰는 방식을 자바에서는 람다라고함 그니까 이거 자바에서도 된다는 소리
 // 화살함수
-console.log( add2(2,3) );
-console.log( add3(2,3) );
+console.log( add2(2,2) );
+console.log( add3(2,2) );
 // 자바에서 메소드는 자바스크립트에서 펑션~
 
 var 모듈 = {
@@ -29,3 +29,47 @@ var 모듈 = {
     기능3 : (a, b) => a + b
 }
 // 기능1, 2, 3 모두 같은말임 다 외워야함.
+console.log(모듈.기능1(1,4));
+console.log(모듈.기능2(1,4));
+console.log(모듈.기능3(1,4));
+
+class 모듈2 {
+    constructor (){}
+    기능1 (a, b) {
+        return a + b
+    };
+}
+
+const 객체 = new 모듈2();
+console.log( 객체.기능1(1,4));
+
+// 자바스크립트는 숫자와 문자를 비교 할 수 있음
+// ==는 값을 비교 하는 것
+// ===는 자료형과 값을 비교하는 것
+a = 1;
+b = "1";
+console.log(a+b)
+if( a == b ){
+    console.log("참");
+} else {
+    console.log("거짓");
+}
+
+if( a === b ){
+    console.log("참");
+} else {
+    console.log("거짓");
+}
+
+for(i=0; i<5; i++){
+    console.log(i);
+}
+console.log(i);
+// 자바스크립트는 변수를 선언하지 않고 사용하면 자동으로 변수를 var로 선언한 상태로 만들어줌 
+// 그러니까 전역변수로 사용 할 수 있음
+// 전역변수로 쓰고 싶지않다면 변수를 선언해주자
+
+for(var j=0; j<5; j++){
+    console.log(j);
+}
+console.log(j);
